113ad6941a0fd069b7b1df349969153961a1750c
yet300 <ghg00rus300@gmail.com>
2026-09-21 14:37:12 +0400

    refactor: move FallingBlocks result UI to screen package

--- game/fallingblocks/src/commonMain/kotlin/ge/yet/game/fallingblocks/di/FallingblocksSessionBindings.kt
+++ game/fallingblocks/src/commonMain/kotlin/ge/yet/game/fallingblocks/di/FallingblocksSessionBindings.kt
@@ -1,13 +1,14 @@
-package ge.yet.game.fallingblocks
+package ge.yet.game.fallingblocks.di
 
 import com.arkivanov.decompose.ComponentContext
-import dev.zacsweers.metro.Binds
 import dev.zacsweers.metro.BindingContainer
+import dev.zacsweers.metro.Binds
 import dev.zacsweers.metro.Provides
 import dev.zacsweers.metro.SingleIn
-import ge.yet.game.fallingblocks.component.game.DefaultFallingBlocksComponentFactory
+import ge.yet.game.fallingblocks.FallingblocksSession
 import ge.yet.game.fallingblocks.audio.FallingBlocksAudioAdapter
 import ge.yet.game.fallingblocks.audio.FallingBlocksAudioPlayer
+import ge.yet.game.fallingblocks.component.game.DefaultFallingBlocksComponentFactory
 import ge.yet.game.fallingblocks.component.game.FallingBlocksComponent
 import ge.yet.game.fallingblocks.component.game.store.DefaultNewGameSeedSource
 import ge.yet.game.fallingblocks.component.game.store.NewGameSeedSource
@@ -21,22 +22,31 @@ import ge.yet.game.fallingblocks.domain.model.FallingBlocksEngine
 import ge.yet.game.fallingblocks.domain.repository.GameCommitWriter
 import ge.yet.game.fallingblocks.domain.repository.GameSnapshotLoader
 import ge.yet.game.fallingblocks.domain.repository.TutorialSeenRepository
-import ge.yet.game.miniapp.metro.MiniAppSessionScope
 import ge.yet.game.miniapp.compose.MiniAppAdsCapability
+import ge.yet.game.miniapp.metro.MiniAppSessionScope
 
 @BindingContainer
 abstract class FallingblocksSessionBindings {
-    @Binds internal abstract fun bindLoader(impl: FallingBlocksPersistence): GameSnapshotLoader
-    @Binds internal abstract fun bindWriter(impl: FallingBlocksPersistence): GameCommitWriter
-    @Binds internal abstract fun bindTutorial(impl: FallingBlocksPersistence): TutorialSeenRepository
-    @Binds internal abstract fun bindSeed(impl: DefaultNewGameSeedSource): NewGameSeedSource
-    @Binds internal abstract fun bindAudio(impl: FallingBlocksAudioAdapter): FallingBlocksAudioPlayer
-    @Binds internal abstract fun bindGameFactory(impl: DefaultFallingBlocksComponentFactory): FallingBlocksComponent.Factory
-    @Binds internal abstract fun bindResultFactory(impl: DefaultResultComponentFactory): ResultComponent.Factory
-    @Binds internal abstract fun bindRootFactory(impl: DefaultRootComponentFactory): RootComponent.Factory
+    @Binds
+    internal abstract fun bindLoader(impl: FallingBlocksPersistence): GameSnapshotLoader
+    @Binds
+    internal abstract fun bindWriter(impl: FallingBlocksPersistence): GameCommitWriter
+    @Binds
+    internal abstract fun bindTutorial(impl: FallingBlocksPersistence): TutorialSeenRepository
+    @Binds
+    internal abstract fun bindSeed(impl: DefaultNewGameSeedSource): NewGameSeedSource
+    @Binds
+    internal abstract fun bindAudio(impl: FallingBlocksAudioAdapter): FallingBlocksAudioPlayer
+    @Binds
+    internal abstract fun bindGameFactory(impl: DefaultFallingBlocksComponentFactory): FallingBlocksComponent.Factory
+    @Binds
+    internal abstract fun bindResultFactory(impl: DefaultResultComponentFactory): ResultComponent.Factory
+    @Binds
+    internal abstract fun bindRootFactory(impl: DefaultRootComponentFactory): RootComponent.Factory
 
     companion object {
-        @Provides internal fun provideEngine(): FallingBlocksEngine = DefaultFallingBlocksEngine
+        @Provides
+        internal fun provideEngine(): FallingBlocksEngine = DefaultFallingBlocksEngine
 
         @Provides
         @SingleIn(MiniAppSessionScope::class)
